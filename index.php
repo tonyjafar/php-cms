88d7926e8065607628a012177f2c9adf33d02fff
Tony Jafar <tjafar@novomind.com>
2018-07-22 13:52:42 +0200

    get posts

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php include "includes/header.php"; ?>
+
+    <?php include "includes/header.php"; ?>
      <!-- Navigation -->
   <?php include "includes/navigation.php"; ?>
     <!-- Page Content -->
@@ -15,20 +16,24 @@
                 </h1>
 
                 <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <?php
+                    $query = "select * from posts order by post_date DESC";
+                    $posts = mysqli_query($conn, $query);
+                    while ($row = mysqli_fetch_assoc($posts)){
+                        echo "<h2><a href='#'>{$row['post_title']}</a></h2>";
+                        echo "<p class='lead'>by <a href='#'>{$row['post_author']}</a></p>";
+                        echo "<p><span class='glyphicon glyphicon-time'></span>{$row['post_date']}</p><hr>";
+                        echo "<img class='img-responsive' src='http://placehold.it/900x300' alt=''><hr>";
+                        echo "<p>{$row['post_content']}</p>";
+                        echo "<a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a><hr>";
+                    }
+                ?>
+                
+                
+                
+                
 
-                <hr>
+                
                 
             </div>
 
