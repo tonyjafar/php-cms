3251ff531d739e778d8324ca213b15940507a3f2
Tony Jafar <tjafar@novomind.com>
2018-07-24 15:39:46 +0200

    category CRUD

---
+++ admin/categories.php
@@ -0,0 +1,126 @@
+<?php include "includes/header.php"; ?>
+<?php include "../includes/db.php"; ?>
+<?php
+if (isset($_POST['Category'])){
+    $cat = mysqli_real_escape_string($conn, $_POST['Category']);
+    if($cat != ""){
+        $query = "insert into categories (cat_title) values ('$cat')";
+        $result = mysqli_query($conn, $query);
+        if (!$result){
+            echo mysqli_error($conn);
+        }
+    }else{
+        $url = "categories.php";
+        header('Location: '.$url);
+    }
+}elseif (isset($_POST['old-Category']) && isset($_POST['new-Category'])){
+    $cat_old = mysqli_real_escape_string($conn, $_POST['old-Category']);
+    $cat_new = mysqli_real_escape_string($conn, $_POST['new-Category']);
+    if($cat_old != "" && $cat_new != ""){
+        $query = "update categories set cat_title = '$cat_new' where cat_title = '$cat_old'";
+        $result = mysqli_query($conn, $query);
+        if (!$result){
+            echo mysqli_error($conn);
+        }
+    }else{
+        $url = "categories.php";
+        header('Location: '.$url);
+    }
+}elseif (isset($_POST['Del-Category'])){
+    $cat = mysqli_real_escape_string($conn, $_POST['Del-Category']);
+    if($cat != ""){
+        $query = "delete from categories where cat_title = '$cat'";
+        $result = mysqli_query($conn, $query);
+        if (!$result){
+            echo mysqli_error($conn);
+        }
+    }else{
+        $url = "categories.php";
+        header('Location: '.$url);
+    }
+}
+?>
+
+    <div id="wrapper">
+
+        <?php include "includes/navi.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+                    </div>
+<div class="col-xs-6">
+<table class='table table-hover'>
+  <thead>
+    <tr>
+      <th scope='col'>#</th>
+      <th scope='col'>ID</th>
+      <th scope='col'>Category</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php
+$catState = "select * from categories";
+$result = mysqli_query($conn, $catState);
+if ($result) {
+    $x = 1;
+    while ($row = mysqli_fetch_assoc($result)){
+        $id = $row['cat_id'];
+        $category = $row['cat_title'];
+        echo "<tr><th scope='row'>$x</th><td>$id</td><td>$category</td></tr>";
+        $x++;
+    }
+    
+}
+?>
+</tbody>
+</table>
+</div>
+</tbody>
+</table>
+<div class="col-xs-6">
+<form action="categories.php" method="post">
+      <div class="form-group">
+       <label for="Category">Add Category</label>
+        <input class="form-control" id="Category" type="text" name="Category" placeholder="Add Category">
+       </div>
+        <input class="btn btn-primary" type="submit" value="Add" name="submit">
+</form>
+</div>
+<br><br>
+<div class="col-xs-6">
+<form action="categories.php" method="post">
+      <div class="form-group">
+       <label for="Category">Old Category</label>
+        <input class="form-control" id="Category" type="text" name="old-Category" placeholder="Old Category">
+       </div>
+       <div class="form-group">
+       <label for="Category">New Category</label>
+        <input class="form-control" id="Category" type="text" name="new-Category" placeholder="New Category">
+       </div>
+        <input class="btn btn-success" type="submit" value="Edit" name="submit">
+</form>
+</div>
+<div class="col-xs-6">
+<form action="categories.php" method="post">
+      <div class="form-group">
+       <label for="Category">Delete Category</label>
+        <input class="form-control" id="Category" type="text" name="Del-Category" placeholder="Delete Category">
+       </div>
+        <input class="btn btn-danger" type="submit" value="Delete" name="submit">
+</form>
+</div>
+</div>
+                </div>
+            </div>
+        </div>
+<?php include "includes/footer.php"; ?>
--- admin/includes/navi.php
+++ admin/includes/navi.php
@@ -43,7 +43,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                        <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
