73ceb5ae82f56b6af5ad259cfe989d698f396c04
Tony Jafar <tjafar@novomind.com>
2018-08-04 21:21:17 +0200

    add comments

---
+++ admin/comments.php
@@ -0,0 +1,59 @@
+<?php include "includes/header.php"; ?>
+<?php include "../includes/db.php"; ?>
+<?php
+if (isset($_GET['acc'])){
+    $id = $_GET['acc'];
+    $acState = "update comments set com_status = 'public' where com_id = '$id'";
+    $acc = mysqli_query($conn, $acState);
+    header("Location: comments.php");
+}elseif (isset($_GET['del'])){
+    $id = $_GET['del'];
+    $delState = "delete from comments where com_id = '$id'";
+    $del = mysqli_query($conn, $delState);
+    header("Location: comments.php");
+}
+?>
+
+    <div id="wrapper">
+
+        <?php include "includes/navi.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small><?php echo $adminname; ?></small>
+                        </h1>
+                    </div>
+                    <table class='table table-hover'>
+  <thead>
+    <tr>
+      <th scope='col'>#</th>
+      <th scope='col'>ID</th>
+      <th scope='col'>User</th>
+      <th scope="col">Content</th>
+    </tr>
+  </thead>
+  <tbody>
+                    <?php
+                    $comsStat = "select * from comments where com_status = 'draft'";
+                    $comments = mysqli_query($conn, $comsStat);
+                    $x = 1;
+                    while ($row = mysqli_fetch_assoc($comments)){
+                        $id = $row['com_id'];
+                        $user_name = $row['user_name'];
+                        $content = $row['com_content'];
+                        echo "<tr><th scope='row'>$x</th><td>$id</td><td>$user_name</td><td>$content</td><td><a class='btn btn-success' href='comments.php?acc={$row['com_id']}'>Accept<span class='glyphicon glyphicon-chevron-right'></span></a><hr></td><td><a class='btn btn-danger' href='comments.php?del={$row['com_id']}'>Delete<span class='glyphicon glyphicon-chevron-right'></span></a><hr></td></tr>";
+                        
+                        $x++;
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+<?php include "includes/footer.php"; ?>
--- post.php
+++ post.php
@@ -1,7 +1,9 @@
 <?php include "includes/header.php"; ?>
 
     <!-- Navigation -->
- <?php include "includes/navigation.php"; ?>
+ <?php include "includes/navigation.php"; 
+
+?>
 
     <!-- Page Content -->
     <div class="container">
@@ -34,14 +36,32 @@
                         $url = "index.php";
                         header('Location: '.$url);
                     }
+                
+                 if (isset($_POST['comment'])){
+                     if ($user -> LoggedIn()){
+                        $comment =  mysqli_real_escape_string($conn, $_POST['comment']);
+                        if ($comment != ""){
+                            $user_name = $_COOKIE['loggedIn'];
+                            $date = date("Y-m-d H:i:s");
+                            $commState = "insert into comments (user_name, post_id, com_date, com_content) ";
+                            $commState .= "values ('$user_name', '$id', '$date', '$comment')";
+                            $result = mysqli_query($conn, $commState);
+                            if ($result){
+                                echo "<h3>Comment sent to Admin to review.</h3>";
+                            }else{
+                                die(mysqli_error($conn));
+                            }
+                        }
+                    }
+                }
                 ?>
 
                 <!-- Blog Comments -->
 
                 <!-- Comments Form -->
                 <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <h4>Leave a Comment: (You need to be logged in :)  )</h4>
+                    <form action= "post.php?id=<?php echo $id;?>" role="form" method="post">
                         <div class="form-group">
                             <textarea class="form-control" rows="3" name="comment"></textarea>
                         </div>
@@ -54,44 +74,20 @@
                 <!-- Posted Comments -->
 
                 <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
-
+                <?php
+                    $comState = "select * from comments where post_id = '$id' and com_status = 'public'";
+                    $comments = mysqli_query($conn, $comState);
+                    while ($row = mysqli_fetch_assoc($comments)){
+                        echo "<div class='media'>";
+                        echo "<a class='pull-left' href='#'>";
+                        echo "<img class='media-object' src='http://placehold.it/64x64' alt=''>";
+                        echo "</a>";
+                        echo "<div class='media-body'>";
+                        echo "<h4 class='media-heading'>{$row['user_name']} <small>{$row['com_date']}</small></h4>";
+                        echo $row['com_content'];
+                        echo "</div></div>";
+                    }
+                ?>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
