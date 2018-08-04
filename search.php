a180f17192ac3ecc8120dccc93b6836efd2406b0
Tony Jafar <tjafar@novomind.com>
2018-08-04 15:54:57 +0200

    search author links

--- search.php
+++ search.php
@@ -25,7 +25,7 @@
                         if (mysqli_num_rows($posts) > 0){
                             while ($row = mysqli_fetch_assoc($posts)){
                                 echo "<h2><a href='post.php?id={$row['post_id']}'>{$row['post_title']}</a></h2>";
-                                echo "<p class='lead'>by <a href='#'>{$row['post_author']}</a></p>";
+                                echo "<p class='lead'>by <a href='user-posts.php?author={$row['post_author']}'>{$row['post_author']}</a></p>";
                                 echo "<p><span class='glyphicon glyphicon-time'></span>{$row['post_date']}</p><hr>";
                                 echo "<img class='img-responsive' src='images/{$row['post_image']}' alt=''><hr>";
                                 echo "<p>{$row['post_content']}</p>";
