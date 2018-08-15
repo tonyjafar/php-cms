a6bd48def611aae8ad2b86684bb6f4b73f34e8af
Tony Jafar <tjafar@novomind.com>
2018-08-15 13:44:54 +0200

    add widgets to dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,23 @@
 <?php include "includes/header.php"; ?>
+     <?php include "../includes/db.php"; ?>
+<?php
+    $postsCount = mysqli_query($conn, "select count(*) from posts");
+    $postsCount = mysqli_fetch_assoc($postsCount);
+    $postsCount = $postsCount['count(*)'];
+    
+    $commentsCount = mysqli_query($conn, "select count(*) from comments");
+    $commentsCount = mysqli_fetch_assoc($commentsCount);
+    $commentsCount = $commentsCount['count(*)'];
 
+    $usersCount = mysqli_query($conn, "select count(*) from users");
+    $usersCount = mysqli_fetch_assoc($usersCount);
+    $usersCount = $usersCount['count(*)'];
+    
+    $catCount = mysqli_query($conn, "select count(*) from categories");
+    $catCount = mysqli_fetch_assoc($catCount);
+    $catCount = $catCount['count(*)'];
+
+?>
     <div id="wrapper">
 
         <?php include "includes/navi.php"; ?>
@@ -17,6 +35,100 @@
                         </h1>
                     </div>
                 </div>
+                       
+                <!-- /.row -->
+                
+<div class="row">
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-file-text fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                  <div class='huge'><?php echo $postsCount; ?></div>
+                        <div>Posts</div>
+                    </div>
+                </div>
+            </div>
+            <a href="all_posts.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-comments fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                     <div class='huge'><?php echo $commentsCount; ?></div>
+                      <div>Comments</div>
+                    </div>
+                </div>
+            </div>
+            <a href="comments.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-yellow">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-user fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                    <div class='huge'><?php echo $usersCount; ?></div>
+                        <div> Users</div>
+                    </div>
+                </div>
+            </div>
+            <a href="all_users.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-red">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-list fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class='huge'><?php echo $catCount; ?></div>
+                         <div>Categories</div>
+                    </div>
+                </div>
+            </div>
+            <a href="categories.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+                <!-- /.row -->
             </div>
         </div>
 <?php include "includes/footer.php"; ?>
